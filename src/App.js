304323445e4a25479450f9bf7ae00b341e007256
SusanaChen03 <grupochen@hotmail.com>
2022-05-03 22:01:30 +0200

    export Conversation function

--- src/App.js
+++ src/App.js
@@ -5,35 +5,17 @@ import Header from './containers/Header/Header.js';
 import Footer from './containers/Footer/Footer';
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Suggestion from './containers/Suggestion/Suggestion';
+import Conversation from './containers/Conversation/Conversation';
 
 
 function App() {
   return (
     <div className="contentAll">
-         <BrowserRouter>
-      <Header/>
-    
-        <div className="conversation">
-            <div className="admin">
-                <div className="avatar"></div>
-                <div className="triangle"></div>
-                <div className="bubble1">¿Estás trabajando actualmente?</div>
-            </div>
-            <div className="user">
-                <div className="bubble2">Actualmente no</div>
-                <div className="triangle2"></div>        
-                <div className="avatarUser"></div>
-            </div>
-            <div className="admin">
-                <div className="avatar"></div>
-                <div className="triangle"></div>
-                <div className="bubble1">¿Qué tecnologías dominas?</div>
-            </div>
-        
-        </div>
-  
-      <Suggestion/>
-      <Footer/>
+      <BrowserRouter>
+        <Header/>
+        <Conversation/>
+        <Suggestion/>
+        <Footer/>
       </BrowserRouter>
     </div>
   );
