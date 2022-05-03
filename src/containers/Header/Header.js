21d8168c35b12ef476ccdce7f5869532537d16ea
SusanaChen03 <grupochen@hotmail.com>
2022-05-03 19:44:58 +0200

    create function Header

---
+++ src/containers/Header/Header.js
@@ -0,0 +1,22 @@
+import "./Header.css"
+
+const Header = () => {
+
+
+    
+    return(
+        <header>
+          
+        <span class="backIcon">
+            <span class="iconify " style="font-size: 26px;" data-icon="eva:arrow-ios-back-fill"></span>
+        </span> 
+
+        <span class="imageIcon">
+            <span class="iconify" style="font-size: 26px;" data-icon="bx:image-add"></span>
+        </span>
+   
+        </header>
+    )
+};
+
+export default Header;
