73adc03d539188d0712cf8db3d1c5d1afb67fb5d
SusanaChen03 <grupochen@hotmail.com>
2022-05-03 21:41:03 +0200

    changes original App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
 import logo from './logo.svg';
 import './App.css';
-
+import jsimages from 'https://code.iconify.design/2/2.2.1/iconify.min.js';
+import Header from './containers/Header/Header.js';
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="contentAll">
+         <BrowserRouter>
+      <Header/>
+     
+
+        <div className="conversation">
+            <div className="admin">
+                <div className="avatar"></div>
+                <div className="triangle"></div>
+                <div className="bubble1">¿Estás trabajando actualmente?</div>
+            </div>
+            <div className="user">
+                <div className="bubble2">Actualmente no</div>
+                <div className="triangle2"></div>        
+                <div className="avatarUser"></div>
+            </div>
+            <div className="admin">
+                <div className="avatar"></div>
+                <div className="triangle"></div>
+                <div className="bubble1">¿Qué tecnologías dominas?</div>
+            </div>
+        
+        </div>
+        <div className="buttons">
+            <div className="yes">Sí, estoy trabajando </div>
+            <div className="no">Actualmente no</div>
+        </div>
+        <div className="footer">
+            <span className="plus">
+                <span className="iconify" style={{fontsize:"40px"}} data-icon="akar-icons:plus"></span>
+            </span>
+            <div  >
+                <input type="text" className="text" />
+            </div>
+            <span className="micro">  
+                <span className="iconify" style={{fontsize:"40px"}} data-icon="clarity:microphone-line"></span>
+            </span>  
+        </div>
+        <Routes>
+         
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
