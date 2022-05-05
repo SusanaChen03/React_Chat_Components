167c181e1ad2729ad3eea5ad0b92e60268179a8e
SusanaChen03 <grupochen@hotmail.com>
2022-05-05 20:16:29 +0200

    create folder redux

---
+++ src/store/store.js
@@ -0,0 +1,17 @@
+import createStore from "redux";
+
+
+
+const initialState = {
+    listConversation: [],
+};
+
+
+
+const reductor = (state = initialState, action) => {
+    
+   
+};
+
+
+export default createStore(reductor);
