009e7130b87f40450f8755ebd2b39b9ee4d2c36f
SusanaChen03 <you@example.com>
2022-05-08 19:47:55 +0200

    App.js clean

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
 import jsimages from 'https://code.iconify.design/2/2.2.1/iconify.min.js';
+import './flicker.css';
 import Header from './containers/Header/Header.js';
 import Footer from './containers/Footer/Footer';
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Suggestion from './containers/Suggestion/Suggestion';
 import Conversation from './containers/Conversation/Conversation';
+import Carousel from './containers/Carousel/Carousel';
 
 
 function App() {
   return (
     <div className="contentAll">
-      <BrowserRouter>
           <Header/>
           <Conversation/>
           <Suggestion/>
+          <Carousel/>
           <Footer/>
-      </BrowserRouter>
     </div>
   );
 }
