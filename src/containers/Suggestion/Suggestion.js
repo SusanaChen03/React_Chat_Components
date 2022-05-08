43a1a01d249b905be44907a3bb96b408552ea158
SusanaChen03 <you@example.com>
2022-05-08 19:30:18 +0200

    container Suggestion done

--- src/containers/Suggestion/Suggestion.js
+++ src/containers/Suggestion/Suggestion.js
@@ -1,14 +1,27 @@
 import "./Suggestion.css";
-
+import { useDispatch } from "react-redux";
+import { actionCreator } from "../../store/actionTypes.js";
+import { ADD_CHATS } from "../../store/typesVar.js";
 
 const Suggestion = () => {
-    return(
-        <div className="buttons">
-            <div className="yes">Sí, estoy trabajando </div>
-            <div className="no">Actualmente no</div>
-        </div>
-    )
-};
 
+  const dispatch = useDispatch();
+
+  function handleClickYes(e) {
+    e.preventDefault();
+    dispatch(actionCreator(ADD_CHATS, { text: "Sí, estoy trabajando" }));
+  };
+  function handleClickNo(e) {
+    e.preventDefault();
+    dispatch(actionCreator(ADD_CHATS, { text: "Actualmente no" }));
+  };
+  return (
+    <div className="buttons">
+      <div className="yes" onClick={handleClickYes}>Sí, estoy trabajando{" "} </div>
+        
+      <div className="no" onClick={handleClickNo}> Actualmente no  </div>
+    </div>
+  );
+};
 
 export default Suggestion;
