6cf477847557f27d44c868a087d077d2154520d8
SusanaChen03 <you@example.com>
2022-05-08 19:44:21 +0200

    Footer done

--- src/containers/Footer/Footer.js
+++ src/containers/Footer/Footer.js
@@ -1,20 +1,42 @@
-import "./Footer.css"
+import "./Footer.css";
+import { useDispatch } from "react-redux";
+import { actionCreator } from "../../store/actionTypes.js";
+import { ADD_CHATS } from "../../store/typesVar.js";
 
 const Footer = () => {
+  const dispatch = useDispatch();
 
-    return(
-        <div className="footer">
-            <span className="plus">
-                <span className="iconify" style={{fontSize:"25px"}} data-icon="akar-icons:plus"></span>
-            </span>
-            <div>
-                <input type="text" className="text" />
-            </div>
-            <span className="micro">  
-                <span className="iconify" style={{fontSize:"25px"}} data-icon="clarity:microphone-line"></span>
-            </span>  
-        </div>
-    )
+  function handleClick(e) {
+    e.preventDefault();
+    console.log(document.getElementById("texts").value);
+    let inputText = document.getElementById("texts").value;
+    document.getElementById("texts").value = "";
+    dispatch(actionCreator(ADD_CHATS, { text: inputText }));
+  }
+
+  return (
+    <div className="footer">
+      <span className="micro">
+        <span
+          className="iconify"
+          style={{ fontSize: "25px" }}
+          data-icon="clarity:microphone-line"
+        ></span>
+      </span>
+      <div>
+        <input type="text" id="texts" className="text" />
+      </div>
+      <span className="send" onClick={handleClick}>
+        <span
+          className="iconify"
+          style={{ fontSize: "25px" }}
+          data-icon="lucide:send"
+        ></span>
+      </span>
+    </div>
+  );
 };
 
 export default Footer;
+
+
